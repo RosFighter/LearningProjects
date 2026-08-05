022285a67a7f54c240ba2702b603b69f7b628d50
RosFighter <rosfighter@yandex.ru>
2026-08-05 13:40:30 +0300

    Учебный проект по C#

---
+++ CSharpCource/CSharpCource_01/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharpCource_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = Console.ReadLine();
+            
+            Console.WriteLine("Привет, " + data + "!");
+        
+        }
+
+        static void TestStrings()
+        { 
+            string name = "abrakadabra";
+
+            bool containsA = name.Contains('a');
+            bool containsE = name.Contains('E');
+
+            Console.WriteLine(containsA);
+            Console.WriteLine(containsE);
+
+            bool endsWithAbra = name.EndsWith("abra");
+            bool startWithAbra = name.StartsWith("abra");
+            Console.WriteLine(endsWithAbra);
+            Console.WriteLine(startWithAbra);
+
+            int indexOfA = name.IndexOf('a', 1);
+            Console.WriteLine(indexOfA);
+
+            int lastIndexOfR = name.LastIndexOf('r');
+            Console.WriteLine(lastIndexOfR);
+
+            Console.WriteLine(name.Length);
+        }
+    }
+}

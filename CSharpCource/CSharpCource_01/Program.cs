fd14e75b0c4617d378ee91f3866a6c04543305a9
RisFighter <rosfighter@yandex.ru>
2026-08-06 20:05:24 +0300

    VS Лекция "Изменение строк"

--- CSharpCource/CSharpCource_01/Program.cs
+++ CSharpCource/CSharpCource_01/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            string data = Console.ReadLine();
+            string nameConcat = string.Concat("My ", "name ", "is ", "John");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = string.Join(" ", "My", "name", "is", "John");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = "My " + "name " + "is " + "John";
+            Console.WriteLine(nameConcat);
+
+            nameConcat = nameConcat.Insert(0, "Hi there! ");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = nameConcat.Remove(0, 3);   // удалит 3 символа, начиная с 0
+            Console.WriteLine(nameConcat);
+
+            string replaced = nameConcat.Replace('n', 'z');
+            Console.WriteLine(replaced);
+
+            replaced = nameConcat.Replace("John", "Ivan!");
+            Console.WriteLine(replaced);
+
+            string data = "67584;87068;5687345;132,8907,35453";
+            string[] splitData = data.Split(";");
+            string first = splitData[0];
+            Console.WriteLine(first);
+
+            char[] chars = nameConcat.ToCharArray();
+            Console.WriteLine(chars[5]);
+            Console.WriteLine(nameConcat[5]);
+
+            string lower = nameConcat.ToLower();
+            Console.WriteLine(lower);
+            string upper = nameConcat.ToUpper();
+            Console.WriteLine(upper);
+
+            string john = " My name is John! ";
+            Console.WriteLine(john.Trim());
+        }
+
+        static void TestEnter()
+        {
+            string data = null;
+            data = Console.ReadLine();
             
             Console.WriteLine("Привет, " + data + "!");
 
